aa856e85102c6a6bb6f3754d333b2b2980eac412
poliukhovychD <f0rgetz@knu.ua>
2023-05-21 22:31:24 +0300

    z Bogom

--- OnlineShoping/OnlineShoping/settings.py
+++ OnlineShoping/OnlineShoping/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ik31=#%z0k*6-(*kef%hom)o%#jky79x2t2dix@0w6l%^0hl0'
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
