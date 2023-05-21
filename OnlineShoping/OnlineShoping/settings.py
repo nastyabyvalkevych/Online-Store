1cffbd24bb220f638e86ec93f8bb6bde2cf33f81
poliukhovychD <f0rgetz@knu.ua>
2023-05-21 21:44:00 +0300

    tests added

--- OnlineShoping/OnlineShoping/settings.py
+++ OnlineShoping/OnlineShoping/settings.py
@@ -33,8 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 
     'main',
-
-
+    'jet.dashboard',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
