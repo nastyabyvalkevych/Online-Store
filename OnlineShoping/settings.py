a14229deaa53e7a419b13a300291ece07c5f35b8
Nastya Byvalkevych <nastuly72@gmail.com>
2023-05-24 12:46:41 +0300

    changes2

--- OnlineShoping/settings.py
+++ OnlineShoping/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PORT = os.environ.get('DB_PORT')
